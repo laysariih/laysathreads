43cc4662430c1a55537ec98f195969bd548802f4
Laysa Ribeiro <laysa.riih@gmail.com>
2024-11-25 02:11:50 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,11 +25,11 @@ cloudinary.config({
 });
 
 // Middlewares
-app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
-app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(express.json({ limit: "50mb" })); // Para analisar dados JSON no req.body
+app.use(express.urlencoded({ extended: true })); // Para analisar dados do formulário em req.body
 app.use(cookieParser());
 
-// Routes
+// Rotas
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
@@ -39,10 +39,10 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	// react app
+// Reage ao aplicativo
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
 
-server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`Servidor iniciado em http://threads.aschopaa.com.br:${PORT}`));
